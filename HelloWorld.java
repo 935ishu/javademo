d1959189250a22fb0eaad81b92008fc1ed948353
935ishu <koushiishu@gmail.com>
2026-06-11 11:09:17 +0530

    java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Docker!");
+        System.out.println("Hello from Docker and java!");
     }
 }
